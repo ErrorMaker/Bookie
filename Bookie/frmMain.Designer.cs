619714b8366ed47f4f2dff411141b00d97d31f08
Scott Stamp <scott@hypermine.com>
2016-04-05 19:13:32 -0230

    Initial commit.

---
+++ Bookie/frmMain.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Bookie
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.ssMain = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.ssMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConnect.Enabled = false;
+            this.btnConnect.Location = new System.Drawing.Point(12, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(167, 43);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Reconnect";
+            this.btnConnect.UseVisualStyleBackColor = false;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // ssMain
+            // 
+            this.ssMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.ssMain.Location = new System.Drawing.Point(0, 67);
+            this.ssMain.Name = "ssMain";
+            this.ssMain.Size = new System.Drawing.Size(191, 22);
+            this.ssMain.TabIndex = 1;
+            this.ssMain.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(117, 17);
+            this.lblStatus.Text = "Status: Disconnected";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(191, 89);
+            this.Controls.Add(this.ssMain);
+            this.Controls.Add(this.btnConnect);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Bookie";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ssMain.ResumeLayout(false);
+            this.ssMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.StatusStrip ssMain;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+    }
+}
+
---
+++ Bookie/frmMain.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sulakore.Communication;
+using Sulakore.Extensions;
+using WebSocketSharp;
+using Sulakore.Protocol;
+
+namespace Bookie
+{
+    public partial class frmMain : ExtensionForm
+    {
+        //WebSocketWrapper socket;
+        WebSocket socket;
+        Dictionary<int, int> replacements = new Dictionary<int, int>();
+        int roomId = 0;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeSocket()
+        {
+            socket = new WebSocket("ws://198.50.128.206:3000/");
+            socket.OnOpen += Socket_OnOpen;
+            socket.OnClose += Socket_OnClose;
+            socket.OnMessage += Socket_OnMessage;
+            socket.Connect();
+        }
+
+        private void Socket_OnClose(object sender, CloseEventArgs e)
+        {
+            ssMain.Invoke((MethodInvoker)delegate
+            {
+                lblStatus.Text = "Status: Disconnected";
+            });
+        }
+
+        private void Socket_OnOpen(object sender, EventArgs e)
+        {
+            ssMain.Invoke((MethodInvoker)delegate
+            {
+                lblStatus.Text = "Status: Connected";
+            });
+        }
+
+        private void Socket_OnMessage(object sender, MessageEventArgs e)
+        {
+            var parameters = e.Data.Split(':');
+            if (parameters[0] == "update")
+            {
+                int id = int.Parse(parameters[1]);
+                int result = int.Parse(parameters[2]);
+                Connection.SendToClientAsync(934, id, result);
+            }
+            if (parameters[0] == "clearfurni")
+                replacements.Clear();
+            if (parameters[0] == "furni")
+            {
+                HMessage msg = new HMessage(StringToByteArray(parameters[1]));
+                msg.Header = 2726;
+                Connection.SendToClientAsync(msg.ToBytes());
+            }
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            Triggers.DetectOutgoing = true;
+            Triggers.DetectIncoming = true;
+            Triggers.OutAttach(1912, DiceRolled);
+            Triggers.OutAttach(2960, DiceClosed);
+            Triggers.OutAttach(3718, EnterRoom);
+            Triggers.InAttach(2726, FurniLoaded);
+
+            InitializeSocket();
+        }
+
+        private void EnterRoom(InterceptedEventArgs e)
+        {
+            roomId = e.Packet.ReadInteger();
+            socket.Send($"loadedroom:{roomId}");
+        }
+
+        public static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
+        private void DiceClosed(InterceptedEventArgs e)
+        {
+            socket.Send($"close:{e.Packet.ReadInteger()}");
+            e.IsBlocked = true;
+        }
+
+        private void FurniLoaded(InterceptedEventArgs e)
+        {
+            e.IsBlocked = true;
+            socket.Send($"loadedroom:{roomId}");
+        }
+
+        private void DiceRolled(InterceptedEventArgs e)
+        {
+            socket.Send($"roll:{e.Packet.ReadInteger()}");
+            e.IsBlocked = true;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            socket.Close();
+            socket.Connect();
+        }
+    }
+}
